f6dc6f830c20b68ad79909680c5d0406eddf84cf
Sylvester Das <sylvester@purelogic.in>
2017-08-21 12:01:59 +0530

    blod - update v1

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -32,3 +32,10 @@
     <!-- Custom styles for this template -->
     <link href='{{ asset("css/app.css") }}' rel="stylesheet">
 @endsection
+
+@section('seo')
+        <title>{{ $post->title }} - Air Quality Index</title>
+        <meta name="keywords" content="blog, air pollution, pollution, air quality, aqi, india, aqi india, pollution control, delhi, north india, ncr, gurgaon, noida, gaziabad, faridabad, enviornment, smog, dust, dust particles">
+        <meta name="author" content="Purelogic Labs">
+        <meta name="robots" content="index/follow">
+@endsection
